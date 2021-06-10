f245209942794b9e294d678719732ad61713cb5a
Moulham Hallak <mou990@gmail.com>
2021-06-10 22:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ class Triangle extends Polygon {
 }
 
 class Square extends Polygon {
-  get isValid() {
-    if(this.countSides === 4){
+  get isValid(){
+    if(this.sides[0] === this.sides[2] && this.sides[1] === this.sides[3] && this.sides[0] === this.sides[1]){
       return true;
+    } else {
+      return false;
     }
   }
   
