937fd147ec190c6f960924351794fb237f7d71a0
Moulham Hallak <mou990@gmail.com>
2021-06-10 22:52:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+class Polygon {
+  constructor(array) {
+    this.sides = array;
+  }
+  
+  get countSides() {
+    return this.sides.length;
+  }
+  
+  get perimeter() {
+    return this.sides.reduce((acc, curr) => acc + curr, 0)
+  }
+}
+
+class Triangle extends Polygon {
+  get isValid() {
+    if(this.countSides === 3){
+      return true;
+    }
+  }
+}
+
+class Square extends Polygon {
+  get isValid() {
+    if(this.countSides === 4){
+      return true;
+    }
+  }
+  
+  get area(){
+    return this.sides.reduce((acc, curr) => acc * curr, 1)
+  }
+}
